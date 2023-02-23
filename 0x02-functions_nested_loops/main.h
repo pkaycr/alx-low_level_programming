ee4a93f3d267671ff91355421d95fd4f8341705a
Peter Kayode <pkaycr@hotmail.com>
2023-02-22 16:27:38 -0800

    Task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * File: "main.h"
+ * Auth: "Boitumelo Tshehla"
+ * Desc: "The header file contains all declarations of functions used in the 0x02-functions_nested_loops directory."
+ */
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,3 +19,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+#endif
